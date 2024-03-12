1ad5046d257d2ea3239f2124a75ce4e59ab42979
Your Name <youremail@yourdomain.com>
2024-03-12 15:07:19 -0400

    aaa

--- models/californiaModel.js
+++ models/californiaModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const joi = require("joi");
+
 const californiaSchema = new mongoose.Schema({
     longitude: String,
     latitude: String,
--- routes/california.js
+++ routes/california.js
@@ -4,29 +4,45 @@ const { CaliforniaModel } = require("../models/californiaModel")
 const router = experss.Router();
 //get california
 
+//funck for change the name lat lon price
+function renameKey(obj, oldKey, newKey) {
+    obj[newKey] = obj[oldKey];
+    delete obj[oldKey];
+}
+
+
+
 router.get("/californiaList", async (req, res) => {
-    let perPage = Math.min(req.query.perPage, 20) || 5;
+    let perPage = Math.min(req.query.perPage, 20) || 500;
     let page = req.query.page - 1 || 0;
     let sort = req.query.sort || "_id";
-    let reverse = req.query.reverse == "yes" ? 1 : -1
-    ;
+    let reverse = req.query.reverse == "yes" ? 1 : -1;
     try {
-        let data = await CaliforniaModel.find({})
-
+        let data = await CaliforniaModel.find({
+            //  longitude: "lat", latitude: "lat", median_house_value: "price"
+        })
+            .select("longitude latitude median_house_value")
             // limiting the pages by default 5 and adding /?perPage=20  will show until 20 //http://localhost:3002/places/?perPage=1
             .limit(perPage)
             //skiping how many records to skip
             .skip(page * perPage)
             //sorting from small to big 1 , from big to small -1 // http://localhost:3002/places/?sort=score&reverse=yes
             .sort({ [sort]: reverse })
-        res.json(data);
-        console.log(data);
+
+        //change name to lat lon price
+        let jsonString = JSON.stringify(data);
+
+        const arr = JSON.parse(jsonString);
+        arr.forEach(data => renameKey(data, 'longitude', 'lon'));
+        arr.forEach(data => renameKey(data, 'latitude', 'lat'));
+        arr.forEach(data => renameKey(data, 'median_house_value', 'price'));
+
+        console.log(arr);
+        res.json(arr);
     }
     catch (err) {
         console.log(err);
     }
-
-
 
 })
 
