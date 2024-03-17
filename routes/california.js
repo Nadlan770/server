8a2f2cef44c5a80e933898f151d581ff49d908f6
Your Name <youremail@yourdomain.com>
2024-03-17 00:58:41 -0400

    aaa

--- routes/california.js
+++ routes/california.js
@@ -11,7 +11,7 @@ function renameKey(obj, oldKey, newKey) {
 }
 
 
-
+//rout bring longitude latitude median_house_value
 router.get("/californiaList", async (req, res) => {
     let perPage = Math.min(req.query.perPage, 20) || 500;
     let page = req.query.page - 1 || 0;
@@ -39,6 +39,62 @@ router.get("/californiaList", async (req, res) => {
 
         // console.log(arr);
         res.json(arr);
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+})
+
+
+
+//rout bring population ocean_proximity households
+router.get("/californiaOtherList", async (req, res) => {
+    let perPage = Math.min(req.query.perPage, 20) || 500;
+    let page = req.query.page - 1 || 0;
+    let sort = req.query.sort || "_id";
+    let reverse = req.query.reverse == "yes" ? 1 : -1;
+    try {
+        let data = await CaliforniaModel.find({
+            //  longitude: "lat", latitude: "lat", median_house_value: "price"
+        })
+            .select("population ocean_proximity households")
+            // limiting the pages by default 5 and adding /?perPage=20  will show until 20 //http://localhost:3002/places/?perPage=1
+            .limit(perPage)
+            //skiping how many records to skip
+            .skip(page * perPage)
+            //sorting from small to big 1 , from big to small -1 // http://localhost:3002/places/?sort=score&reverse=yes
+            .sort({ [sort]: reverse })
+
+        // console.log(data);
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+})
+
+//rout bring full list
+router.get("/californiaFullList", async (req, res) => {
+    let perPage = Math.min(req.query.perPage, 20) || 500;
+    let page = req.query.page - 1 || 0;
+    let sort = req.query.sort || "_id";
+    let reverse = req.query.reverse == "yes" ? 1 : -1;
+    try {
+        let data = await CaliforniaModel.find({
+            //  longitude: "lat", latitude: "lat", median_house_value: "price"
+        })
+            // .select("population ocean_proximity households")
+            // limiting the pages by default 5 and adding /?perPage=20  will show until 20 //http://localhost:3002/places/?perPage=1
+            .limit(perPage)
+            //skiping how many records to skip
+            .skip(page * perPage)
+            //sorting from small to big 1 , from big to small -1 // http://localhost:3002/places/?sort=score&reverse=yes
+            .sort({ [sort]: reverse })
+
+        // console.log(data);
+        res.json(data);
     }
     catch (err) {
         console.log(err);
