928f0964ea9c925c81c51bdb62387365b44f2e8b
Your Name <youremail@yourdomain.com>
2024-03-19 11:54:12 -0400

    users adedd

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,7 +1,9 @@
+const indexR = require("./index");
 const usersR = require("./users");
 const californiaR = require("./california");
 
 exports.routesInit = (app) => {
+    app.use("/", indexR);
     app.use("/users", usersR);
     app.use("/california", californiaR);
 }
